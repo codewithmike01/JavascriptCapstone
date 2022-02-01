4cbee0f2b4749eb7f12395e91cbfedf5d82f7886
Mwapsam <mwapsam@gmail.com>
2022-02-01 17:19:13 +0200

    render data dynamically from an API

--- src/module/renderStruc.js
+++ src/module/renderStruc.js
@@ -1,8 +1,17 @@
-export default function renderStructure(data) {
-  const name = data.name;
-  const id = data.id;
+const renderStructure = (data) => {
+  const { name } = data;
+  const { id } = data;
   const img = data.sprites.other['official-artwork'].front_default;
-  // The images, id and name can be accesed now.
-  // You can work with them using the varible above, to render your work
-  console.log(img);
-}
+
+  const list = document.getElementById('list-items');
+  list.innerHTML += `<div class="grid-item">
+  <img src="${img}" alt="" />
+  <div class="space-item">
+    <h4>${name} ${id}</h4>
+    <i class="far fa-heart"></i>
+  </div>
+  <button type="button" class="btn">Comments</button>
+</div>`;
+};
+
+export default renderStructure;
