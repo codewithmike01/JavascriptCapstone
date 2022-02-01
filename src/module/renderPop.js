7d621071d1ca6b529e7daa2fc27b7fcfaa242ef4
ginohmk <michotall95@gmail.com>
2022-02-01 21:44:20 +0100

    yes

--- src/module/renderPop.js
+++ src/module/renderPop.js
@@ -1,11 +1,31 @@
 const popUps = document.querySelector('.pop-ups');
+const buttonStruct = document.querySelector('.grid-container');
+const gridItem = document.querySelector('.grid-container');
 let popCards = '';
+
+function popUpToggle(id) {
+  const popItems = popUps.querySelectorAll('.popup-info');
+
+  popItems.forEach((item) => {
+    if (id === item.id) {
+      popUps.classList.toggle('hide');
+      item.classList.toggle('hide');
+      gridItem.classList.toggle('isDisable');
+    }
+  });
+}
+
 export default function renderPopUp(data) {
-  const name = data.name;
-  const id = data.id;
+  console.log(data);
+  const moves = data.moves['0'].move.name;
+  const { weight } = data;
+  const { height } = data;
+  const ability = data.abilities[0].ability.name;
+  const { name } = data;
+  const { id } = data;
   const img = data.sprites.other['official-artwork'].front_default;
 
-  popCards += ` <div class="popup-info" id='${id}'>
+  popCards += ` <div class="popup-info hide" id='${id}'>
   <div class="image">
     <img src="${img}" alt="" />
     <div class="close">&times;</div>
@@ -14,12 +34,12 @@ export default function renderPopUp(data) {
     <h3>${name}</h3>
     <div class="details-info">
       <div class="left">
-        <h4>Fuel: titanium</h4>
-        <h4>Length: 100,000</h4>
+        <h4>Move: ${moves}</h4>
+        <h4>Length: ${height}cm</h4>
       </div>
       <div class="right">
-        <h4>Weight:400</h4>
-        <h4>Power: 100,000,000</h4>
+        <h4>Weight: ${weight}pounds</h4>
+        <h4>Power: ${ability}</h4>
       </div>
     </div>
   </div>
@@ -49,14 +69,21 @@ export default function renderPopUp(data) {
   popUps.innerHTML = popCards;
 }
 
+// Event listeners
+
+buttonStruct.addEventListener('click', (event) => {
+  if (event.target.className === 'btn') {
+    // eslint-disable-next-line prefer-destructuring
+    const id = event.target.parentNode.id;
+    popUpToggle(id);
+  }
+});
+
 popUps.addEventListener('click', (event) => {
   if (event.target.className === 'close') {
     const parentHit = event.target.parentNode.parentNode;
+    popUps.classList.toggle('hide');
     parentHit.classList.toggle('hide');
+    gridItem.classList.toggle('isDisable');
   }
 });
-//   item.addEventListener('click', () => {
-//     console.log('hello');
-//     document.querySelector('.pop-ups').classList.toggle('hide');
-//   });
-// });
